cbbc4e02f29685d9ce2172b8f88300086c89eee7
GauravChambhare <gauravchambhare236@gmail.com>
2026-06-11 20:29:38 +0530

    Add solution: CutTheSticks

--- JAVA_beginner_DSA_foundations_prerequisites/step1/CutTheSticks.java
+++ JAVA_beginner_DSA_foundations_prerequisites/step1/CutTheSticks.java
@@ -5,43 +5,33 @@ import java.math.*;
 import java.util.regex.*;
 
 
+
+
 public class CutTheSticks {
-    
-    final static int reducer(ArrayList<Integer> currentPile){
-        int currentSize = currentPile.size();
-        System.out.println(currentSize); // current size of pile before each iteration
-        if(currentSize==0) return 0;
-        // main reducing logic
-        // find the smallest stick size first
-        int smallestSize = Integer.MAX_VALUE;
-        for(int i=0; i<currentSize; i++){
-            if(smallestSize>currentPile.get(i)){
-                smallestSize = currentPile.get(i); // tracking the current smallest value in pile
-            }
-        }
-        // now substracting this value from all of the value of currentPile
-        for(int i=0; i<currentSize; i++){
-            currentPile.set(i, currentPile.get(i) - smallestSize);
-        }
-        // now all values in currentPile that are zero has to be removed/cleared.
-        currentPile.removeIf(element -> element.equals(0));
-        return currentPile.size();
-        // return current size of remaining pile post removal of all 0 le ngth sticks
-    }
     public static void main(String args[]) {
-      
       Scanner in = new Scanner(System.in);
-      int noOfStickes = in.nextInt();
-      ArrayList<Integer> stickPile = new ArrayList<>(noOfStickes);
-      for(int i=0; i<noOfStickes; i++){
-        stickPile.add(in.nextInt());
+      int n  = in.nextInt(); 
+      int[] arr = new int[n];
+      for(int i = 0 ;i<n;i++){
+          arr[i] = in.nextInt();
       }
-    // I have now created my stick pile
-    // I can now either write code directly in my main function or in helper function
-    while(noOfStickes>0){
-        noOfStickes = reducer(stickPile);
+      // Optimised approach
+      //Sorting the array first in O(nlogn)
+      Arrays.sort(arr);
+      int totalSticksRemaining = n;
+      int curr = arr[0]; // smallest value remaining after sorting, this will keep getting updated as we move to next smallest values,i.e value greater just greater than curr
+      int curr_count = 0; // count of curr(current element), initialized to 1 as curr is present once.
+      System.out.println(totalSticksRemaining); // printing initial no of sticks
+      for(int i=0; i<n; i++){
+        if(curr==arr[i]){
+            curr_count++;
+        } else { // no is different than previous number
+            totalSticksRemaining -= curr_count; // remaing sticks will be 
+            curr_count = 1;
+            curr = arr[i];
+            System.out.println(totalSticksRemaining);
+        }
+      }
+      return;
     }
-    
-    return;
-  }  
 }
