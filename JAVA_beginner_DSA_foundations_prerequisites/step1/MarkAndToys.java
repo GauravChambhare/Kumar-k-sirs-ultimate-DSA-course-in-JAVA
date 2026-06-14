3e48b2331f55af4e73dc56ab060ea8522c98864d
GauravChambhare <gauravchambhare236@gmail.com>
2026-06-14 10:31:26 +0530

    Added solution: MarkAndToys

---
+++ JAVA_beginner_DSA_foundations_prerequisites/step1/MarkAndToys.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class MarkAndToys {. //tc O(nlogn) and sc = O(1) if input storing is not considered.
+    public static void main(String args[]) {
+      Scanner in = new Scanner(System.in);
+      int n, k ; 
+      n = in.nextInt(); // no of toys on sale
+      k = in.nextInt(); // budget
+      int[] arr= new int[n];
+      for(int i=0; i<n; i++){
+          arr[i] = in.nextInt();
+      }
+      // sorting the prices in ascending order
+      Arrays.sort(arr);
+      int currentSum=0;
+      int ans=0;
+      for(int i=0; i<n; i++){
+        if(currentSum<=k){
+            currentSum += arr[i];
+            ans = currentSum<=k ? ans+1 : ans +0; // it is imp to check this post current current sum is updated. 
+            // as it might happen that we did currentSum +=arr[i] and not it has exceeded buget, and without any checking if budget has exceeded we are still incrementing ans
+        } else {
+            break;
+        }
+      }
+      System.out.println(ans);
+      return;
+    }
+}
