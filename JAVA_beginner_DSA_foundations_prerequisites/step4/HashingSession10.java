74001deb7170b137fdebbbad6ac6977fe2281410
GauravChambhare <gauravchambhare236@gmail.com>
2026-07-13 21:36:18 +0530

    Added HashingSession 10 problem

--- JAVA_beginner_DSA_foundations_prerequisites/step4/HashingSession10.java
+++ JAVA_beginner_DSA_foundations_prerequisites/step4/HashingSession10.java
@@ -1,5 +1,8 @@
 package JAVA_beginner_DSA_foundations_prerequisites.step4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashingSession10 {
 
     public static int noOfSubarraysBruteForce(int arr[], int k){
@@ -19,10 +22,29 @@ public class HashingSession10 {
         }
         return ans;
     }
+
+    public static int noOfSubarraysOptimized(int arr[], int k){
+        int ans=0;
+        // although we are going to follow prefix sum approach, we dont necessarily need to declare a 
+        // prefix sum array, instead we can use a temp var 
+        Map<Integer, Integer> cSM = new HashMap<>();
+        int currentSum = 0;
+        // we are adding the base case below, which happens when the sum till now is ==k
+        cSM.put(0, 1);
+        for(int num : arr){
+            currentSum += num;
+            if(cSM.containsKey(currentSum-k)){  //i.e 
+                ans += cSM.get(currentSum-k);
+            }
+            cSM.put(currentSum, cSM.getOrDefault(currentSum, 0)+1);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         
         int[] arr = new int[]{2, 4, 5, 8, 1, 6, 3};
-        int k = 10; // i.e count no of subarrays whose sum of elements = 10
+        int k = 6; // i.e count no of subarrays whose sum of elements = 6 | 2, 4 & 6
 
         System.out.println(noOfSubarraysBruteForce(arr, k));
         return;
