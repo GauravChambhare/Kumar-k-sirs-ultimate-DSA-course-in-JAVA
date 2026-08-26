69be7e202475277aa11e019759ba65a3fa954c35
GauravChambhare <gauravchambhare236@gmail.com>
2026-08-26 10:10:48 +0530

    Modified .gitignore file to ignore .class files and .vscode directory

---
+++ JAVA_beginner_DSA_foundations_prerequisites/step4/Session5.java
@@ -0,0 +1,11 @@
+package JAVA_beginner_DSA_foundations_prerequisites.step4;
+
+public class Session5 {
+ /*
+ Given an array “nums” of size “N” - find the total number of pairs(i,j) [i<j] such that (nums[i] + nums[j])%5 = 0 (nums[i]>=0 for all i) 
+ */   
+  public static int totalPairs(int[] nums) {
+    
+    
+}
+}
